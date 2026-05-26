dfe37fd9a5826181dcbfc705d6b0ba06331f4f7e
RASIT <shjh0815@naver.com>
2026-05-26 15:46:26 +0900

    fix: 잘못된 테스트 기댓값 수정

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -4,11 +4,11 @@
 TEST(CalculatorTest, AddPositiveNumbers)
 {
     EXPECT_EQ(add(2, 3), 5);
-    EXPECT_EQ(add(3, 4), 6);
+    EXPECT_EQ(add(3, 4), 7);
 }
 
 TEST(CalculatorTest, AddNegativeNumbers)
 {
-    EXPECT_EQ(add(-2, -1), 5);
-    EXPECT_EQ(add(-3, 4), 6);
+    EXPECT_EQ(add(-2, -1), -3);
+    EXPECT_EQ(add(-3, 4), 1);
 }
