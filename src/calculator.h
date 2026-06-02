f38d841ee86d8536286b5155ce5a47b7d0c04ece
RASIT <shjh0815@naver.com>
2026-06-02 17:35:12 +0900

    fix: calculator.h 함수 선언을 include guard 안으로 이동

--- src/calculator.h
+++ src/calculator.h
@@ -1,11 +1,8 @@
-//
-// Created by SSAFY on 26. 5. 18..
-//
-
 #ifndef HTTP_SERVER_CALCULATOR_H
 #define HTTP_SERVER_CALCULATOR_H
 
-#endif //HTTP_SERVER_CALCULATOR_H
-
 int add(int a, int b);
 int divide(int a, int b);
+
+#endif //HTTP_SERVER_CALCULATOR_H
+
