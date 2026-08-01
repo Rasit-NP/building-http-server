e1c9a382377abd4f441c02df4c3fe8921988a143
RASIT <shjh0815@naver.com>
2026-08-02 02:35:42 +0900

    test: HttpResponse serialize 단위 테스트 추가
    - HttpResponseTest.cpp 추가, TestWithParam<ResponseCase> 기반 value-parameterized 구성
    - 200 OK / 400 Bad Request 응답의 serialize() 출력을 기대 문자열과 바이트 단위로 비교
    - INSTANTIATE_TEST_SUITE_P로 케이스 인스턴스화, ResponseNameGenerator로 테스트명 부여
    - 전역 심볼을 ResponseCases/ResponseNameGenerator로 명명해 기존 파서 테스트의 cases와 충돌 방지
    - tests/CMakeLists.txt에 responses 라이브러리와 HttpResponseTest.cpp 등록

---
+++ tests/HttpResponseTest.cpp
@@ -0,0 +1,48 @@
+# include <string>
+# include <gtest/gtest.h>
+# include "http/HttpResponse.h"
+
+struct ResponseCase {
+    int status;
+    std::string body;
+    std::string expected;
+};
+
+class ResponseTest : public testing::TestWithParam<ResponseCase> {
+protected:
+    HttpResponse res;
+};
+
+struct ResponseNameGenerator {
+    std::string operator()(
+        const testing::TestParamInfo<ResponseCase>& info) const {
+        return "ResponseTest_" + std::to_string(info.param.status);
+    }
+};
+
+std::vector<ResponseCase> ResponseCases = {
+    {200, "Success",
+        "HTTP/1.1 200 OK\r\n"
+        "Connection: close\r\n"
+        "Content-Length: 7\r\n"
+        "\r\n"
+        "Success"},
+    {400, "Bad Request",
+        "HTTP/1.1 400 Bad Request\r\n"
+        "Connection: close\r\n"
+        "Content-Length: 11\r\n"
+        "\r\n"
+        "Bad Request"}
+};
+
+INSTANTIATE_TEST_SUITE_P(Normal, ResponseTest, testing::ValuesIn(ResponseCases), ResponseNameGenerator());
+
+TEST_P(ResponseTest, ResponseTest) {
+    const auto& c = GetParam();
+
+    res.status_code = c.status;
+    res.headers.emplace_back("Connection", "close");
+    res.body = c.body;
+
+    EXPECT_EQ(res.serialize(), c.expected);
+}
